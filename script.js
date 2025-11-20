e2e5a284108408f2585ceac44dbcccc80c92e40f
JaiderCiro <jaider.rendon.313@gmail.com>
2025-11-20 17:02:10 -0500

    Se realiza ejercicio #2 (map con objetos)

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ console.log(puntos);
 let puntosDoble = puntos.map(numero => numero * 2);
 console.log(puntosDoble);
 
+//Ejercicio #2
+
+let usuarios = [{id: 1, nombre: "Ana"}, {id: 2, nombre: "Luis"}, {id: 3, nombre: "Carlos"}];
+console.log(usuarios);
+
+let nombres = usuarios.map(usuarios => usuarios.nombre);
+console.log(nombres);
